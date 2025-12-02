24c6ac9470834976969ab58069b19c75cc66ee59
Mosharraf Hosain <mosharraf.hosain@bkash.com>
2025-12-02 11:59:13 +0600

    bump service worker version

--- 2025/assets/js/common.js
+++ 2025/assets/js/common.js
@@ -319,7 +319,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Centralized Service Worker registration for all pages in /2025/
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('service-worker.js?v19123200' )
+            navigator.serviceWorker.register('service-worker.js?v19123205' )
                 .then(registration => {
                     console.log('[SW] Registered with scope:', registration.scope);
 
