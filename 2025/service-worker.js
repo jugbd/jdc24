8f305f2b1312a4a0c1b7ec3a569acf810227910f
Bazlur Rahman <bazlur@jugbd.org>
2025-12-02 12:36:47 -0500

    Bump service worker cache version

--- 2025/service-worker.js
+++ 2025/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache-v176138785215'; // ⬅️ bump this on each deploy to bust cache
+const CACHE_NAME = 'cache-v176138785216'; // ⬅️ bump this on each deploy to bust cache
 
 // ---- INSTALL ----
 self.addEventListener('install', event => {
